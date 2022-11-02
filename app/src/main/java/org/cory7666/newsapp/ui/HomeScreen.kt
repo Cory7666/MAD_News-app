53e66c1449384bef1a45e13d3756288b278b83d0
Alex Alexeev <activity.alex69985@gmail.com>
2022-11-02 13:16:56 +0300

    home screen: add sign out button

--- app/src/main/java/org/cory7666/newsapp/ui/HomeScreen.kt
+++ app/src/main/java/org/cory7666/newsapp/ui/HomeScreen.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import org.cory7666.newsapp.R
 
 class HomeScreen : Fragment()
@@ -13,6 +16,12 @@ class HomeScreen : Fragment()
     inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
   ): View?
   {
-    return inflater.inflate(R.layout.fragment_home_screen, container, false)
+    return inflater
+      .inflate(R.layout.fragment_home_screen, container, false)
+      ?.also {
+        it.findViewById<Button>(R.id.button).setOnClickListener {
+          Firebase.auth.signOut()
+        }
+      }
   }
 }
