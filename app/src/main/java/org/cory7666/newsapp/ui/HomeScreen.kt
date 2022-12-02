1cde3501f65e1b1a3c05e80dcf50383d79183ffc
Alex Alexeev <activity.alex69985@gmail.com>
2022-12-02 11:04:33 +0300

    add custom home action bar

--- app/src/main/java/org/cory7666/newsapp/ui/HomeScreen.kt
+++ app/src/main/java/org/cory7666/newsapp/ui/HomeScreen.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import org.cory7666.newsapp.R
 import org.cory7666.newsapp.databinding.FragmentHomeScreenBinding
+import org.cory7666.newsapp.viewmodel.ActionBarViewModel
 import org.cory7666.newsapp.viewmodel.HomeScreenViewModel
 
 class HomeScreen : Fragment()
@@ -26,12 +30,25 @@ class HomeScreen : Fragment()
     viewModel = ViewModelProvider(this)[HomeScreenViewModel::class.java]
     binding.recyclerView.layoutManager = LinearLayoutManager(context)
     binding.recyclerView.adapter = HomeRecyclerAdapter(viewModel.newsList, this)
-    activity?.actionBar?.show()
+
+    setupActionBar()
 
     CoroutineScope(Dispatchers.Main).launch {
       viewModel.update()
     }
 
     return binding.root
+  }
+
+  private fun setupActionBar()
+  {
+    ViewModelProvider(requireActivity())[ActionBarViewModel::class.java].setCustomBarAndShow(
+      R.layout.home_screen_action_bar
+    )
+    requireActivity().actionBar?.customView
+      ?.findViewById<Button>(R.id.buttonGoToSettings)
+      ?.setOnClickListener {
+        findNavController().navigate(R.id.action_homeScreen_to_settingsScreen)
+      }
   }
 }
