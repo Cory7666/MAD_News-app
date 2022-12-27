3e8f9e95b7bd00d4bf1e3b2a5369da8b141bbbf7
Alex Alexeev <activity.alex69985@gmail.com>
2022-12-27 11:24:27 +0300

    edit class

--- app/src/main/java/org/cory7666/newsapp/data/news/NewsInfo.kt
+++ app/src/main/java/org/cory7666/newsapp/data/news/NewsInfo.kt
@@ -1,4 +1,3 @@
 package org.cory7666.newsapp.data.news
 
-class NewsInfo(val title: String, val description: String, val url: String)
-{}
+data class NewsInfo(val title: String, val description: String, val url: String)
